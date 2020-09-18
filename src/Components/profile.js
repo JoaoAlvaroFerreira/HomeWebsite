e9155943f30a3088d0cb2b211ea1a366dd1efd8c
JoaoAlvaroFerreira <joaoalvaro.ferreira@hotmail.com>
2020-09-18 17:46:52 +0100

    build

--- src/Components/profile.js
+++ src/Components/profile.js
@@ -6,23 +6,32 @@ import React from 'react';
 class Profile extends React.Component {
     render() {
         return (
-            <div class="d-flex justify-content-center mt-5">
-            <div class="container">
-            <div class="row">
-            <h2 class="h2 mx-auto">João Álvaro Ferreira</h2></div>
-    
-            <div class="row">
-            <img class="rounded-circle z-depth-2 mx-auto" alt="100x100" src="https://mdbootstrap.com/img/Photos/Avatars/img%20(31).jpg"
-              data-holder-rendered="true"/>
-              </div>
-              <div class="row">
-                  ABOUT ME
-              </div>
-              </div>
+            <div class="d-flex justify-content-center mt-3 mb-3">
+                <div class="container">
+                    <div class="row">
+                        <div class="column">
+                        <img class="rounded-circle z-depth-2 mx-auto" style={{width:'60%'}} src="https://mdbootstrap.com/img/Photos/Avatars/img%20(31).jpg"
+                        data-holder-rendered="true" />
+                        </div>
+                        <div class="column ml-5">
+                            <div class="row">
+                        <h2 class="h2 mx-auto">João Álvaro Ferreira</h2></div>
+                        <div class="row">
+                            About me
+                        </div>
+                        </div>
+                        </div>
+
+                   
+                </div>
+
+                
             </div>
-            );
-        }
-    
+
+
+        );
+    }
+
 }
 
 export default Profile;
